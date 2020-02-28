edbf7305259f9ea71cf74b8e5a7b443dcb28da2e
Naudria <rima.jean@gmail.com>
2020-02-28 11:48:50 -0600

    vertical nav cont

--- js/custom.js
+++ js/custom.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#sidebarCollapse').toggleClass('reverse');
+        $('.close-button').toggle();
     });
 
 });
