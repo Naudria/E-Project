a33af21e72138dbe856f4a917a25ada73a7d6903
Naudria <rima.jean@gmail.com>
2020-02-28 14:32:01 -0600

    compiled scss

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,3 @@
-// var state = "minimized";
-// // //Check if navbar is expanded or minimized and handle 
-// let subMenu = document.querySelectorAll("#submenu")
-
-// $('#navbar-toggle').click(function() {
-//     if (state == "expanded") {
-//         $('.sidebar').css('margin-left', '-180px');
-//         $(subMenu).css('display', 'none');
-//         state = "minimized";
-//     } else {
-//         if (state == "minimized") {
-//             $('.sidebar').css('margin-left', '0px');
-//             $(subMenu).css('display', 'block');
-//             state = "expanded";
-//         }
-//     }
-// });
 
 $(document).ready(function () {
 
