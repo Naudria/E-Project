0d79e8cb97e0e3a19d9f3f80a832bc35554d61db
Naudria <rima.jean@gmail.com>
2020-02-28 09:23:57 -0600

    vertical nav changes

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,27 @@
-var state = "minimized";
-// //Check if navbar is expanded or minimized and handle 
-let subMenu = document.querySelectorAll("#submenu")
+// var state = "minimized";
+// // //Check if navbar is expanded or minimized and handle 
+// let subMenu = document.querySelectorAll("#submenu")
 
-$('#navbar-toggle').click(function() {
-    if (state == "expanded") {
-        $('.sidebar').css('margin-left', '-180px');
-        $(subMenu).css('display', 'none');
-        state = "minimized";
-    } else {
-        if (state == "minimized") {
-            $('.sidebar').css('margin-left', '0px');
-            $(subMenu).css('display', 'block');
-            state = "expanded";
-        }
-    }
+// $('#navbar-toggle').click(function() {
+//     if (state == "expanded") {
+//         $('.sidebar').css('margin-left', '-180px');
+//         $(subMenu).css('display', 'none');
+//         state = "minimized";
+//     } else {
+//         if (state == "minimized") {
+//             $('.sidebar').css('margin-left', '0px');
+//             $(subMenu).css('display', 'block');
+//             state = "expanded";
+//         }
+//     }
+// });
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('reverse');
+    });
+
 });
 
